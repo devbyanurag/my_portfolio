018e775ef1402d1bfdf1637cb84eb187d6022bdf
anuragchhetri <karanchheti@gmail.com>
2024-04-26 11:22:41 +0530

    added links

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import styles from "./Profile.module.css";
-import profileImg from '../../assets/images/project/profile.jpg'
+import profileImg from '../../assets/images/project/profile.png'
 import linkedInImg from '../../assets/images/social/linkedin.png'
 import gitImg from '../../assets/images/social/github.png'
 
@@ -9,6 +9,8 @@ import emailImg from '../../assets/images/social/email.png'
 import locImg from '../../assets/images/social/loc.png'
 import calImg from '../../assets/images/social/cal.png'
 import downloadImg from '../../assets/images/social/download.png'
+import resumePdf from '../../assets/datafiles/ANURAG CHHETRI resume.pdf'
+
 
 
 
@@ -23,16 +25,23 @@ const Profile = () => {
 
                 </div>
                 <div className={styles.socialHandleContainer}>
-                    <img className={styles.imageContainer} src={linkedInImg} alt="Linkedin" />
-                    <img className={styles.imageContainer} src={gitImg} alt="Linkedin" />
+                    <a href="https://in.linkedin.com/in/anuragchhetri" target="_blank" rel="noopener noreferrer">
+                        <img className={styles.imageContainer} src={linkedInImg} alt="Linkedin" />
+                    </a>
+                    <a href="https://github.com/devbyanurag" target="_blank" rel="noopener noreferrer">
+
+                        <img className={styles.imageContainer} src={gitImg} alt="github" />
+                    </a>
                 </div>
 
             </div>
             <div className={styles.profile_info_2}>
                 <div className={styles.profile_info_container}>
-                    <div className={styles.profile_info_image_container}>
-                        <img src={phoneImg} alt="phone" />
-                    </div>
+                    <a href="tel:+917455898088">
+                        <div className={styles.profile_info_image_container}>
+                            <img src={phoneImg} alt="phone" />
+                        </div>
+                    </a>
                     <div className={styles.profile_info}>
                         <p>Phone</p>
                         <p>+91 7455898088</p>
@@ -40,20 +49,23 @@ const Profile = () => {
                 </div>
                 <div className={styles.hr1}></div>
                 <div className={styles.profile_info_container}>
-                    <div className={styles.profile_info_image_container}>
-                        <img src={emailImg} alt="email" />
-                    </div>
+                    <a href="mailto:example@example.com">
+                        <div className={styles.profile_info_image_container}>
+                            <img src={emailImg} alt="email" />
+                        </div>
+                    </a>
                     <div className={styles.profile_info}>
                         <p>Email</p>
                         <p>anuragchhetri49@gmail.com</p>
                     </div>
                 </div>
                 <div className={styles.hr1}></div>
-               
+
                 <div className={styles.profile_info_container}>
-                    <div className={styles.profile_info_image_container}>
+                    <a href="https://maps.app.goo.gl/H7B5YzrMBjNgr57p9" target="_blank" rel="noopener noreferrer"><div className={styles.profile_info_image_container}>
                         <img src={locImg} alt="Location" />
                     </div>
+                    </a>
                     <div className={styles.profile_info}>
                         <p>Location</p>
                         <p>Dehradun, Uttarakhand</p>
@@ -70,10 +82,11 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.download_container}>
+            <a className={styles.resumedownload} href={resumePdf} download><div className={styles.download_container}>
                 <img src={downloadImg} alt="download" />
                 <p>Download CV</p>
             </div>
+            </a>
         </div>
     )
 }
